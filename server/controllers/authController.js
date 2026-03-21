6cdf5f22d4c611305e515b122032a78ea33a0720
ujjawal <ubhalla0428@gmail.com>
2026-03-21 23:53:03 +0530

    feat: implement auth system with JWT and PostgreSQL

---
+++ server/controllers/authController.js
@@ -0,0 +1,54 @@
+//SIGNUP
+const bcrypt = require("bcrypt");
+const { createUser, findUserByEmail } = require("../models/userModel");
+
+exports.signup = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if user exists
+    const existingUser = await findUserByEmail(email);
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await createUser(email, hashedPassword);
+
+    res.status(201).json({ message: "User created", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+//LOGIN
+const jwt = require("jsonwebtoken");
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      "secretkey",
+      { expiresIn: "1d" }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("StudyForge API running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
+const pool = require("./config/db");
+
+pool.connect()
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));

cdbe411b7ae411f4b5652d9c27f727712ccab387
ujjawal <ubhalla0428@gmail.com>
2026-03-22 02:11:05 +0530

    fix:codebase improvization

--- server/index.js
+++ server/index.js
@@ -1,33 +1,34 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const pool = require("./config/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+const pomodoroRoutes = require("./routes/pomodoroRoutes");
+const journalRoutes = require("./routes/journalRoutes");
+
 app.get("/", (req, res) => {
   res.send("StudyForge API running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
-const authRoutes = require("./routes/authRoutes");
 app.use("/api/auth", authRoutes);
-const pool = require("./config/db");
+app.use("/api/tasks", taskRoutes);
+app.use("/api/pomodoro", pomodoroRoutes);
+app.use("/api/journal", journalRoutes);
 
 pool.connect()
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
-const taskRoutes = require("./routes/taskRoutes");
 
-app.use("/api/tasks", taskRoutes);
-
-const pomodoroRoutes = require("./routes/pomodoroRoutes");
-
-app.use("/api/pomodoro", pomodoroRoutes);
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
 
 const journalRoutes = require("./routes/journalRoutes");
 
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 const { signup, login } = require("../controllers/authController");
 
 router.post("/signup", signup);
 router.post("/login", login);
-
-module.exports = router;
-const authMiddleware = require("../middleware/authMiddleware");
 router.get("/protected", authMiddleware, (req, res) => {
   res.json({
     message: "Protected route accessed successfully",
     user: req.user,
   });
 });
+
+module.exports = router;
